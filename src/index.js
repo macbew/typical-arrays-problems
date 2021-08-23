28226f045fcba8576d9c74d9396967e7da9c6d52
macbew <62144432+macbew@users.noreply.github.com>
2021-08-23 20:50:14 +0300

    add full function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  function compareNumbers(a, b) {
+    return a - b;
+  }
+  if(array==null||array.length===0){ 
+    return 0; 
+   }
+    else{ array.sort(compareNumbers);
+      return array[0];}
+  }
 
 exports.max = function max (array) {
-  return 0;
-}
+  function compareNumbers(a, b) {
+    return a - b;
+  }
+  if(array==null||array.length===0){ 
+    return 0; 
+   }
+    else{ array.sort(compareNumbers);
+      return array[array.length-1];}
+  }
+
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  function compareNumbers(a, b) {
+    return a - b;
+  }
+  if(array==null||array.length==0){  return 0;
+   }
+    else{   
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+ return ((array.reduce(reducer))/(array.length));}
+  }
